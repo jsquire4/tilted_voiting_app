121ca2a9b1987c55d632c454da514803b8014463
jsquire4 <jacobmsquire@yahoo.com>
2017-10-30 19:06:06 -0400

    added fs to package.json

--- models/vote.js
+++ models/vote.js
@@ -24,7 +24,7 @@ var voteSchema = new Schema({
 var Vote = module.exports = mongoose.model('Vote', voteSchema);
 
 function russianNameGenerator(){
-  var fakeNames = JSON.parse(fs.readFileSync("./russianNames.json"));
+  var fakeNames = JSON.parse(fs.readFileSync("../russianNames.json"));
   var firstnames = fakeNames[0].firstnames;
   var lastnames = fakeNames[1].lastnames;
   var firstname = firstnames[Math.floor(Math.random()*firstnames.length)].Name;
@@ -39,7 +39,7 @@ function userNameGenerator(russianName){
 }
 
 function commentGenerator(){
-  var fakedComments = JSON.parse(fs.readFileSync("./fakedComments.json"));
+  var fakedComments = JSON.parse(fs.readFileSync("../fakedComments.json"));
   var comment = fakedComments[Math.floor(Math.random()*fakedComments.length)].Comment;
   return comment;
 }
